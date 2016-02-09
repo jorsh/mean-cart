09d2a34cbfa3be0e6abf093077ab52a160dbaea0
jorsh <jorshx@gmail.com>
2016-02-08 18:34:04 -0600

    Fixed PUT and DELETE methods

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 router.get('/', function (req, res, next) {
@@ -18,6 +19,14 @@ var Task = mongoose.model('Task', TaskSchema);
 // API 
 mongoose.connect('mongodb://localhost/todo');
 
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to DB')
+});
+
+
 app.get('/tasks', function (req, res, next) {
     Task.find(function(err, tasks){
       if(err){return next(err)}
@@ -26,22 +35,39 @@ app.get('/tasks', function (req, res, next) {
     })
   });
 
+
+// POST
+app.use(bodyParser.json()); 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 app.post('/task', function (req, res, next) {
   var task = new Task(req.body);
-
-    task.save(function(err, tasks){
-      if(err){return next(err)}
-        console.log(tasks);
-      res.json(tasks);
-    })
+  console.log(req.body);
+  task.save(function(err, tasks){
+    if(err){return next(err)}
+      console.log(tasks);
+    res.json(tasks);
+  })
   });
 
 app.put('/task/:id', function (req, res) {
-  res.send('PUT _id task');
+Task.findById(req.params.id, function(err, task){
+        task.name = req.body.name;
+
+        task.save(function(err){
+            if(err){res.send(err)}
+            
+            res.json(task);
+        })
+    })
 });
 
 app.delete('/task/:id', function (req, res) {
-  res.send('DELETE _id Taks');
+  Task.findByIdAndRemove(req.params.id, function(err){
+      if(err){res.send(err)}
+          res.json({message: 'Task has been removed'});
+  })
 });
 
 app.listen(3000, function () {
