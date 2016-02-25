a5a7651aabc69455e644889ed8b3b86b46b04f47
jorsh <jorshx@gmail.com>
2016-02-24 18:22:46 -0600

    Extracted REST services to a factory

--- public/landing/index.js
+++ public/landing/index.js
@@ -1,6 +1,6 @@
 module.exports = angular.module('todo.home',[])
-    .controller('LandingController',['$http', require('./controller-landing')]);
-    //.service('RestService',['$http',require('./service-rest')]);
+    .controller('LandingController',['RestService', require('./controller-landing')])
+    .factory('RestService',['$http','$q','CONSTANTS',require('./service-rest')]);
 //  .directive('myDirective',['someDep',require('./my-directive')])
 //  .factory('myFactory',['someDep',require('./my-factory')])
 //  .filter('myFilter',['someDep',require('./my-filter')])
--- public/landing/service-rest.js
+++ public/landing/service-rest.js
@@ -1,21 +1,135 @@
+
 'use strict';
-function RestService($http){
-    var self = this;
-    self.heading = 'My List';
-    self.tasks = ['item1'];
+/**
+ * Service to implement HTTP methods
+ *
+ * @category  factory
+ * @author    Jorge Arenas
+ * @example   Inject the service as dependency and use like this
+ *
+ RestService.request('GET', 'url/posts', {get: query}, {post: params})
+ .then(function(response) {
+		console.log(response.data);
+	}, function(error) {
+		console.log(error);
+});
+ *
+ *
+ */
+function RestSrv($http,$q,CONSTANTS){
+    return {
+        request : request
+    };
 
-    $http({
-        method: 'GET',
-        url: 'http://localhost:3000/tasks'
-    }).then(function successCallback(response) {
-        self.tasks = response.data;
-        console.log(self.tasks)
-        // this callback will be called asynchronously
-        // when the response is available
-    }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-    });
+    function request(method, url, params, data){
+        var deferred = $q.defer();
 
+        $http({
+            method: method,
+            url: CONSTANTS.API_URL + url,
+            params: params,
+            data: data
+        }).then(function(data) {
+            if (!data.config) {
+                console.error('Server Error');
+            }
+            deferred.resolve(data);
+        }, function(error) {
+            deferred.reject(error);
+        });
+
+        return deferred.promise;
+    } //End request Function
 }
-module.exports = RestService;
+
+module.exports = RestSrv;
+
+//'use strict';
+//function RestService($http){
+//    var self = this;
+//    self.getTasks = function(){
+//        $http({
+//            method: 'GET',
+//            url: 'http://localhost:3000/tasks'
+//        }).then(function successCallback(response) {
+//            self.tasks = response.data;
+//            console.log(self.tasks);
+//            // this callback will be called asynchronously
+//            // when the response is available
+//        }, function errorCallback(response) {
+//            // called asynchronously if an error occurs
+//            // or server returns response with an error status.
+//        });
+//    };
+//
+//    function addTask(){
+//        $http({
+//            method: 'POST',
+//            url: 'http://localhost:3000/task',
+//            data: {name: self.txtTask}
+//        }).then(function successCallback(response) {
+//            //self.tasks = response.data;
+//            console.log(self.tasks);
+//            self.txtTask = '';
+//            self.getTasks();
+//            // this callback will be called asynchronously
+//            // when the response is available
+//        }, function errorCallback(response) {
+//            // called asynchronously if an error occurs
+//            // or server returns response with an error status.
+//        });
+//    }
+//
+//    self.removeTask = function(index){
+//        var taskId = self.tasks[index]._id;
+//        $http({
+//            method: 'DELETE',
+//            url: 'http://localhost:3000/task/' + taskId
+//        }).then(function successCallback(response) {
+//            //self.tasks = response.data;
+//            console.log(self.tasks);
+//            self.getTasks();
+//            // this callback will be called asynchronously
+//            // when the response is available
+//        }, function errorCallback(response) {
+//            // called asynchronously if an error occurs
+//            // or server returns response with an error status.
+//        });
+//    };
+//
+//
+//    self.preEditTask = function(index) {
+//        self.txtTask = self.tasks[index].name;
+//        self.postLabel = 'Edit Task';
+//        self.action = 'edit';
+//        self.taskId = self.tasks[index]._id;
+//    };
+//
+//    self.postAction = function () {
+//        if(self.action === 'edit'){
+//            editTask(self.taskId);
+//            self.postLabel = 'Add Task';
+//            self.txtTask = '';
+//        } else {
+//            addTask();
+//        }
+//    };
+//
+//    function editTask(taskId){
+//
+//        $http({
+//            method: 'PUT',
+//            url: 'http://localhost:3000/task/' + taskId,
+//            data: {name: self.txtTask}
+//        }).then(function successCallback(response) {
+//            self.getTasks();
+//            // this callback will be called asynchronously
+//            // when the response is available
+//        }, function errorCallback(response) {
+//            // called asynchronously if an error occurs
+//            // or server returns response with an error status.
+//        });
+//    }
+//}
+//
+//module.exports = RestService;
